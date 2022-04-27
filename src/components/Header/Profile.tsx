275b4f706cf7a80c50080b8aee64616045f4ba79
Jefferson Ferreira <jefferson14489@gmail.com>
2022-04-27 19:18:40 -0300

    initial commit

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,19 @@
+import { Flex, Box, Avatar, Text } from "@chakra-ui/react";
+
+export function Profile() {
+  return (
+    <Flex align="center">
+      <Box mr="4" textAlign="right">
+        <Text>Jefferson Ferreira</Text>
+        <Text color="gray.300" fontSize="small">
+          jefferson14489@gmail.com
+        </Text>
+      </Box>
+      <Avatar
+        size="md"
+        name="Jefferson Ferreira"
+        src="https://github.com/stringercj12.png"
+      />
+    </Flex>
+  );
+}
